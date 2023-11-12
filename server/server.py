fa9107192e1ac604e2b98500634bf15de798a158
nishkarsh25 <117291117+nishkarsh25@users.noreply.github.com>
2023-11-12 18:33:37 +0530

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    try:
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        bhk = int(request.form['bhk'])
+        bath = int(request.form['bath'])
+    except KeyError as e:
+        return jsonify({'error': f'Missing required form data: {e.args[0]}'}, 400)
+    except ValueError:
+        return jsonify({'error': 'Invalid data format. Ensure total_sqft is a number, and bhk and bath are integers.'}, 400)
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
